170eadaa70acf0f9876346975fd9542a8ddedf05
fenny7646 <fennyvarghese1@gmail.com>
2025-01-09 15:32:55 +0530

    C4- Initial frontend responce 'hey'

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 def base(request):
     return render(request, 'base.html')
+
+def index(request):
+    return HttpResponse("hey")
